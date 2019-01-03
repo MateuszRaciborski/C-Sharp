5f19e22de5d8c36d2dbc28f21df4028adc574c0d
MateuszRaciborski <45817017+MateuszRaciborski@users.noreply.github.com>
2019-01-03 11:52:13 +0100

    poprawki plików

--- KonwerterBASE64/KonwerterBASE64/Program.cs
+++ KonwerterBASE64/KonwerterBASE64/Program.cs
@@ -13,10 +13,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace KonwerterBase64
